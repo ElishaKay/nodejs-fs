11048dabdf91133d8069bace2ec172eb5fad547f
elishakay <kramer1346@gmail.com>
2018-10-23 16:47:51 +0300

    logging arguments and files in directory

--- search.js
+++ search.js
@@ -1,9 +1,25 @@
 fs = require('fs');
 
+// print process.argv
+process.argv.slice(2).forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+});
+
+function readContent(callback) {
+    fs.readFile("./Index.html", function (err, content) {
+        if (err) return callback(err)
+        callback(null, content)
+    })
+}
+
 fs.readdir(process.cwd(), function (err, files) {
   if (err) {
     console.log(err);
     return;
   }
   console.log(files);
+});
+
+readContent(function (err, content) {
+    console.log(content)
 });
