16abc209bdb60242a4f402161242cf7159231d92
elishakay <kramer1346@gmail.com>
2018-10-23 18:12:16 +0300

    printing contents of all files in directory

--- search.js
+++ search.js
@@ -22,21 +22,24 @@ console.log('fileType',fileType)
 console.log('stringToSearchFor',stringToSearchFor);
 
 
-function readContent(callback) {
-    fs.readFile("./Index.html", function (err, content) {
-        if (err) return callback(err)
-        callback(null, content)
+function readContent(filePath) {
+    fs.readFile(filePath, 'utf8', function (err, content) {
+        if (err){
+         	console.log(err)
+        } else {
+        	console.log(content);
+        }
     })
 }
 
 fs.readdir(process.cwd(), function (err, files) {
-  if (err) {
-    console.log(err);
-    return;
+  	if (err) {
+   		console.log(err);
+  	} else {
+		for (let i = 0; i < files.length; i++) {
+			readContent(files[i]);
+		}
+		console.log(files);
   }
-  console.log(files);
 });
 
-readContent(function (err, content) {
-    console.log('content: ',content)
-});
